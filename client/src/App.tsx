539f34885cd487c736a768a60f7d38d4ecbcf030
yejunian <leeye51456@gmail.com>
2024-12-03 22:29:28 +0900

    🚧 실험: 포맷 검사에 실패하는 변경사항 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -38,14 +38,7 @@ function MainContainer({ children }: { children: React.ReactNode }) {
 	const location = useLocation();
 
 	// 중앙 정렬이 필요한 페이지들
-	const centerJustifyRoutes = [
-		"/login",
-		"/join",
-		"/checkPassword",
-		"/profileUpdate",
-		"/emailRegistration",
-		"/oauth",
-	];
+	const centerJustifyRoutes = ["/login","/join","/checkPassword","/profileUpdate","/emailRegistration","/oauth"];
 
 	if (location.pathname === "/") {
 		return <>{children}</>; // 메인 페이지에서는 children만 렌더링
